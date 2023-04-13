3f0100d4f97f1918ecf526127ce69398a7882b66
hugolopesxr <hugo_xr@hotmail.com>
2023-04-13 14:22:07 -0300

    alterando o arquivo json para o que estava no link da atividade.

--- questao_03.js
+++ questao_03.js
@@ -1,21 +1,21 @@
 
 
 const dataJson = require("./json_questao_03.json");
-const faturamentosDiarios = dataJson.faturamentos;
+const faturamentosDiarios = dataJson;
   
   const faturamentoMaximo = faturamentosDiarios.reduce((acumulador, atual) => {
-     return acumulador.faturamento > atual.faturamento ? acumulador : atual;
+     return acumulador.valor > atual.valor ? acumulador : atual;
   });
   
   const faturamentoMinimo = faturamentosDiarios.reduce((acumulador, atual) => {
-     return acumulador.faturamento < atual.faturamento ? acumulador: atual;
+     return atual.valor !== 0 && atual.valor < acumulador.valor ? atual : acumulador;
   });
   
-  const totalFaturamento = faturamentosDiarios.reduce((acumulador, atual) => acumulador + atual?.faturamento, 0);
+  const totalFaturamento = faturamentosDiarios.reduce((acumulador, atual) => acumulador + atual?.valor, 0);
   
   const mediaDoFaturammentoNoMes = totalFaturamento / faturamentosDiarios.length;
   
-  const valoresMaioresQueAMediaDoMes = faturamentosDiarios.filter(data => data.faturamento > mediaDoFaturammentoNoMes)
+  const valoresMaioresQueAMediaDoMes = faturamentosDiarios.filter(data => data.valor > mediaDoFaturammentoNoMes)
   
-  const dadosDoFaturamento = {faturamentoMaximo: faturamentoMaximo, faturamentoMinimo: faturamentoMinimo, totalFaturamento: totalFaturamento, mediaDoFaturammentoNoMes:mediaDoFaturammentoNoMes.toFixed(2), valoresMaioresQueAMediaDoMes: valoresMaioresQueAMediaDoMes}
+  const dadosDoFaturamento = {faturamentoMaximo: faturamentoMaximo, faturamentoMinimo: faturamentoMinimo, totalFaturamento: totalFaturamento, mediaDoFaturammentoNoMes:mediaDoFaturammentoNoMes, valoresMaioresQueAMediaDoMes: valoresMaioresQueAMediaDoMes}
   console.log('dadosDoFaturamento',dadosDoFaturamento);
